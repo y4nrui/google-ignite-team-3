b80d090916e4f39c024380f15706c0d6a18ed7d1
jasminelim327 <jasminelim327@gmail.com>
2021-10-17 09:50:07 +0800

    change to communty page

---
+++ app/lib/communityPage.dart
@@ -0,0 +1,22 @@
+// import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+       child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
--- app/lib/community_page/bottom_nav_bar_cubit.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class BottomNavBarCubit extends Cubit<int> {
-  BottomNavBarCubit() : super(0);
-
-  void selectTab(int index) => emit(index);
-}
