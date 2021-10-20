eea9f65fb774e27f5505ca3d6a469558d48676b3
aiweiii <aiwei.ong.2020@scis.smu.edu.sg>
2021-10-20 12:31:01 +0800

    updated main.dart to lead to dashboard instead of homepage
    I couldn't find a way to use navigator to route from home to dashboard but I commented the original code out (that leads to homepage) so feel free to use it

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,8 @@
+// PLEASE NOTE: I changed main.dart to route to dashboard first
+// SCROLL DOWN for the code that leads to HOMEPAGE
 import 'package:flutter/material.dart';
-import '../generatediphone11prox1widget/GeneratedIPhone11ProX1Widget.dart';
+// import '../generatediphone11prox1widget/GeneratedIPhone11ProX1Widget.dart';
+import 'package:app/dashboard.dart';
 
 void main() {
   runApp(HomepageApp());
@@ -13,13 +16,37 @@ class HomepageApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/GeneratedIPhone11ProX1Widget',
-      routes: {
-        '/GeneratedIPhone11ProX1Widget': (context) =>
-            GeneratedIPhone11ProX1Widget(),
-      },
+      debugShowCheckedModeBanner: false,
+      home: Dashboard(),
     );
   }
 }
+
+
+// BELOW is the ORIGINAL CODE that leads to the HOMEPAGE
+
+// import 'package:flutter/material.dart';
+// import '../generatediphone11prox1widget/GeneratedIPhone11ProX1Widget.dart';
+
+// void main() {
+//   runApp(HomepageApp());
+// }
+
+// class HomepageApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       initialRoute: '/GeneratedIPhone11ProX1Widget',
+//       routes: {
+//         '/GeneratedIPhone11ProX1Widget': (context) =>
+//             GeneratedIPhone11ProX1Widget(),
+//       },
+//     );
+//   }
+// }
 
 
