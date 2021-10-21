c7a5b3da1c0fac4838f935dac50275b08fc5235f
Szetong123 <63947413+Szetong123@users.noreply.github.com>
2021-10-21 10:44:11 +0800

    add homepage_org
    in case needed

--- app/lib/Homepage.dart
+++ app/lib/Homepage.dart
@@ -1,35 +1,27 @@
 import 'package:flutter/material.dart';
 
-
 class HomepageWidget extends StatelessWidget {
-          @override
-          Widget build(BuildContext context) {
-          // Figma Flutter Generator HomepageWidget - FRAME
-            return Container(
-      width: 375,
-      height: 812,
-      decoration: BoxDecoration(
-          color : Color.fromRGBO(255, 255, 255, 1),
-  ),
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-        top: 10,
-        left: 4,
-        child: Container(
-        width: 366,
-        height: 790,
+  @override
+  Widget build(BuildContext context) {
+    // Figma Flutter Generator HomepageWidget - FRAME
+    return Container(
+        width: 375,
+        height: 812,
         decoration: BoxDecoration(
-          image : DecorationImage(
-          image: AssetImage('assets/images/Image1.png'),
-          fit: BoxFit.fitWidth
-      ),
-  )
-      )
-      ),
-        ]
-      )
-    );
-          }
-        }
-        
+          color: Color.fromRGBO(255, 255, 255, 1),
+        ),
+        child: Stack(children: <Widget>[
+          Positioned(
+              top: 10,
+              left: 4,
+              child: Container(
+                  width: 366,
+                  height: 790,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/Image1.png'),
+                        fit: BoxFit.fitWidth),
+                  ))),
+        ]));
+  }
+}
